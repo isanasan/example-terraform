2456706744e0376d1525c2c882abafcf41ec8adb
isanasan <yfcgpsebp@gmail.com>
2023-02-28 17:05:15 +0900

    3.3 use output

--- main.tf
+++ main.tf
@@ -4,17 +4,11 @@ provider "aws" {
   region                  = "ap-northeast-1"
 }
 
-locals {
-    example_instance_type = "t3.micro"
-}
-
 resource "aws_instance" "example" {
     ami = "ami-0c3fd0f5d33134a76"
-    instance_type = local.example_instance_type
+    instance_type = "t3.micro"
+}
 
-    user_data = <<EOF
-        #!/bin/bash
-        yum install -y httpd
-        systemctl start httpd.service
-EOF
+output "example_instance_id" {
+    value = aws_instance.example.id
 }
