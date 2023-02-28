db927b0520d6683abb2ab000dcc5c121b79fa8f3
isanasan <yfcgpsebp@gmail.com>
2023-02-28 17:02:15 +0900

    3.2 use locals

--- main.tf
+++ main.tf
@@ -4,13 +4,13 @@ provider "aws" {
   region                  = "ap-northeast-1"
 }
 
-variable "example_instance_type" {
-    default = "t3.micro"
+locals {
+    example_instance_type = "t3.micro"
 }
 
 resource "aws_instance" "example" {
     ami = "ami-0c3fd0f5d33134a76"
-    instance_type = var.example_instance_type
+    instance_type = local.example_instance_type
 
     user_data = <<EOF
         #!/bin/bash
