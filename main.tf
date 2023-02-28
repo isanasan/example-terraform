00d0bb652d90c0a02179a5cface47ecf5edeb625
isanasan <yfcgpsebp@gmail.com>
2023-02-28 12:00:21 +0900

    add: ec2

--- main.tf
+++ main.tf
@@ -3,3 +3,8 @@ provider "aws" {
   profile                 = "terraform-example"
   region                  = "ap-northeast-1"
 }
+
+resource "aws_instance" "example" {
+    ami = "ami-0c3fd0f5d33134a76"
+    instance_type = "t3.micro"
+}
