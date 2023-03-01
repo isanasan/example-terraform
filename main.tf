7d8ddad7b62c73319c2252b251b29f972b0cc2c8
isanasan <yfcgpsebp@gmail.com>
2023-03-01 14:24:10 +0900

    5.2 add iam_role

--- main.tf
+++ main.tf
@@ -24,3 +24,26 @@ resource "aws_iam_policy" "example" {
   name   = "example"
   policy = data.aws_iam_policy_document.allow_describe_regions.json
 }
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+
+  }
+}
+
+resource "aws_iam_role" "example" {
+  name               = "example"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "example" {
+  role       = aws_iam_role.example.name
+  policy_arn = aws_iam_policy.example.arn
+}
+
