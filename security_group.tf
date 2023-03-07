5b747f34b301d0d35d13fb7a81bb62db90d0e10b
isanasan <yfcgpsebp@gmail.com>
2023-03-07 13:55:17 +0900

    8.2 add ALB

--- security_group.tf
+++ security_group.tf
@@ -1,7 +1,24 @@
-module "example_sg" {
+module "http_sg" {
   source      = "./security_group"
-  name        = "module-sg"
+  name        = "http-sg"
   vpc_id      = aws_vpc.example.id
-  port        = "80"
+  port        = 80
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+module "https_sg" {
+  source      = "./security_group"
+  name        = "https-sg"
+  vpc_id      = aws_vpc.example.id
+  port        = 443
+  cidr_blocks = ["0.0.0.0/0"]
+
+}
+
+module "http_redirect_sg" {
+  source      = "./security_group"
+  name        = "http-redirect-sg"
+  vpc_id      = aws_vpc.example.id
+  port        = 443
   cidr_blocks = ["0.0.0.0/0"]
 }
